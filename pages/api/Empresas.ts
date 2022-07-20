6a00489ba757efa65f572c7df3c56c44f00976b1
Kauã <kauanv06@gmail.com>
2022-07-20 20:52:53 -0300

    API

--- pages/api/Empresas.ts
+++ pages/api/Empresas.ts
@@ -30,6 +30,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json({response})
   } else {
-    res.status(400).json({Error: 'Wrong Method Request'})
+    res.status(400).json({Error: 'Wrong Method Request.'})
   }
 }
