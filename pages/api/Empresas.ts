994c68fc2471647ee6aab38303ae7bba15157e43
Kauã <kauanv06@gmail.com>
2022-07-20 20:45:11 -0300

    API

---
+++ pages/api/Empresas.ts
@@ -0,0 +1,35 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import connect from '../../utils/database'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { CNPJ, Nome, CEP, Endereco, Numero, Bairro, selectUf, Cidade } = req.body
+
+  if (req.method === 'POST') {
+
+    const MyQyery = {
+      CNPJ,
+      Nome,
+      CEP,
+      Endereco,
+      Numero,
+      Bairro,
+      UF: req.body.selectUf,
+      Cidade
+    }
+
+    const { db } = await connect()
+
+    const response = await db.collection('Empresas').insertOne(MyQyery)
+
+
+    res.status(200).json({ response })
+  } else if (req.method === 'GET') {
+    const { db } = await connect()
+    const response = await db.collection('Empresas').find().toArray()
+
+    res.status(200).json({response})
+  } else {
+    res.status(400).json({Error: 'Wrong Method Request'})
+  }
+}
